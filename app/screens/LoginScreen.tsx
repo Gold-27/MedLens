22a96af091d763bb964d06e90d69cd0dcb4aee7a
Holluwartosin <isamuel75@gmail.com>
2026-04-16 01:27:04 +0100

    n

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -18,10 +18,9 @@ const LoginScreen = ({ navigation }: LoginScreenProps) => {
         return true;
       };
 
-      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+      const backHandler = BackHandler.addEventListener('hardwareBackPress', onBackPress);
 
-      return () =>
-        BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+      return () => backHandler.remove();
     }, [])
   );
 
