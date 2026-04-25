20122e5dd0948c1e56d726c0a4ddd49993d35971
Momoh Olasunkanmi <momoholasunkanmi077@gmail.com>
2026-04-25 21:24:57 +0100

    commit

--- api/check.ts
+++ api/check.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const url = process.env.SUPABASE_URL;
-const key = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const url = process.env.SUPABASE_URL!;
+const key = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 const supabase = createClient(url, key);
 
 async function check() {
